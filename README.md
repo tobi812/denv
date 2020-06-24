bcb88e60786f9cbc1df8846d09c964d0919bcdd2
Tobias Rosenstock <tobi.rosenstock@gmail.com>
2020-06-24 13:07:55 +0200

    Add cli tool (#1)
    * WIP - add golang command

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # denv
 Denv is a tool that makes working with multiple docker-compose files simpler.
+
+## Setup development environment
+
+1. Install _golang_
+
+2. Define environment variables
+
+3. Install dependencies:
+
+```
+go get -d ./...
+go install     
+```
+
+4. Compile and run denv
+```
+go build 
+denv
+```
---
+++ denv.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Definition struct {
+	Name  string
+	Files []string
+	path  string
+}
+
+type DenvFile struct {
+	Version     string
+	Environment struct {
+		Name        string
+		Definitions []Definition
+		Boot        []string
+	}
+}
+
+func main() {
+	file, error := os.Open("denv.yaml")
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	defer file.Close()
+
+	data, error := ioutil.ReadAll(file)
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	denvFile := DenvFile{}
+	error1 := yaml.Unmarshal([]byte(data), &denvFile)
+	if error1 != nil {
+		log.Fatalf("error: %v", error1)
+	}
+
+	flag.Parse()
+
+	if len(os.Args) == 1 {
+		fmt.Println("Print HELP")
+		os.Exit(1)
+	}
+
+	switch os.Args[2] {
+	case "up":
+		if len(os.Args) < 3 {
+			fmt.Println("Expected service name.")
+			os.Exit(1)
+		}
+
+		service, definitionError := getDefinition(os.Args[2], denvFile)
+		if definitionError != nil {
+			os.Exit(1)
+		}
+
+		args := []string{"up"}
+
+		for _, file := range service.Files {
+			args = append(args, "-f")
+			args = append(args, file)
+		}
+
+		execCommand("docker-compose", args...)
+	default:
+		fmt.Println("Expected subcommand.")
+		os.Exit(1)
+	}
+	// fmt.Printf("--- t:\n%v\n\n", denvFile)
+	// fmt.Println("Name: " + denvFile.Environment.Name)
+}
+
+func execCommand(name string, args ...string) {
+	cmd := exec.Command(name, args...)
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	outStr, errStr := string(stdout.Bytes()), string(stderr.Bytes())
+
+	if err != nil {
+		fmt.Printf(errStr)
+	} else {
+		fmt.Printf(outStr)
+	}
+}
+
+func getDefinition(name string, denvFile DenvFile) (Definition, error) {
+	for _, definition := range denvFile.Environment.Definitions {
+		if definition.Name == name {
+			return definition, nil
+		}
+	}
+
+	errorMessage := "Definition for service '" + name + "' not found!"
+	fmt.Println(errorMessage)
+
+	return Definition{}, errors.New(errorMessage)
+}
