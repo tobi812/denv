39e021ca2c964080d334e6df994b39806985d2f7
tobias.rosenstock <tobi.rosenstock@gmail.com>
2020-10-16 16:37:31 +0200

    add config cmd

--- cmd/config.go
+++ cmd/config.go
@@ -29,7 +29,7 @@ import (
 	"io/ioutil"
 )
 
-// configCmd represents the printConfig command
+// configCmd represents the config command
 var configCmd = &cobra.Command{
 	Use:   "config",
 	Short: "Print global denv configuration.",
@@ -53,14 +53,4 @@ func printConfig() {
 
 func init() {
 	rootCmd.AddCommand(configCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// configCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// configCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
